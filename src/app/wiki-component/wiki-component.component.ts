fc60bcfcbe121f788014a909d48d9642a4bf22fc
Velizar Atanasov <vatanasov@sbnd.int>
2016-11-11 10:01:10 +0200

    service with obs

--- src/app/wiki-component/wiki-component.component.ts
+++ src/app/wiki-component/wiki-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WikiServiceService } from './wiki-service.service';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-wiki-component',
@@ -8,14 +9,24 @@ import { Observable } from 'rxjs/Rx';
 })
 export class WikiComponentComponent implements OnInit {
 
-  constructor(private wikipediaService: WikiServiceService) { }
+  constructor(private wikipediaService: WikiServiceService) {
+    this.items = this.searchTermStream.debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap((term:string) => this.wikipediaService.search(term));
+   }
 
   ngOnInit() {
   }
 
   items: Observable<string[]>;
-  search (term: string) {
-    this.items = this.wikipediaService.search(term);
+  private searchTermStream = new Subject<string>();
+
+  search(term: string) {
+    this.searchTermStream.next(term);
   }
+
+  // search (term: string) {
+  //   this.items = this.wikipediaService.search(term);
+  // }
 
 }
