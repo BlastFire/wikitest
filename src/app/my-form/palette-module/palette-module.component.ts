48d6810c43c71dd5cc990cdb2a8bd64989d5a6f0
Velizar Atanasov <vatanasov@sbnd.int>
2017-01-20 16:21:17 +0200

    refactor pallete

--- src/app/my-form/palette-module/palette-module.component.ts
+++ src/app/my-form/palette-module/palette-module.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+export interface VSelectModel {
+  name: string,
+  value: string,
+  selected: boolean
+}
+
 @Component({
   selector: 'app-palette-module',
   templateUrl: './palette-module.component.html',
@@ -9,14 +15,22 @@ export class PaletteModuleComponent implements OnInit {
 
   @Output() dmx = new EventEmitter();
 
-  p1Options: { name: string, value: string, selected: boolean}[] = [
-    {name: "v0", value: "0", selected: false},
-    {name: "v1", value: "1", selected: true}
-  ];
+  p1Options: VSelectModel[];
+
+  p2Options: VSelectModel[];
 
   constructor() { }
 
   ngOnInit() {
+    this.p1Options = [
+       {name: "v0", value: "0", selected: false},
+       {name: "v1", value: "1", selected: false}
+    ];
+
+    this.p2Options = [
+       {name: "z0", value: "0", selected: false},
+       {name: "z1", value: "1", selected: false}
+    ];
   }
 
   setSelectedP1(selectElement) {
@@ -29,6 +43,32 @@ export class PaletteModuleComponent implements OnInit {
       }
 
       console.log(this.p1Options);
+  }
+
+  setSelectedP2(selectElement) {
+      for (let i = 0; i < selectElement.options.length; i++) {
+          let optionElement = selectElement.options[i];
+          let optionModel = this.p2Options[i];
+
+          if (optionElement.selected == true) { optionModel.selected = true; }
+          else { optionModel.selected = false; }
+      }
+
+      console.log(this.p2Options);
+  }  
+
+  leftArrow() {
+
+  }
+
+  rightArrow() {
+    //1.vzemi vsichko ot left .. filtrirai po selected i tezi za koito e true gi prehvyrli v dqsnoto.
+    //2. iztrii ot lqvoto prehvyrlenite
+    if(this.p1Options.keys.length != 0) {
+      this.p1Options = this.p1Options.filter((obj: VSelectModel) => {
+        
+      });
+    }
   }
 
   onClicked() {
